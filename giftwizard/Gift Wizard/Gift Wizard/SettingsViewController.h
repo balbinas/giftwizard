b3f824aa38baeb07a76a5a7fe8f4c6570504abc7
balberry <balbinasantana@gmail.com>
2015-06-29 20:39:38 -0500

    nose, screwup listo para merge

---
+++ giftwizard/Gift Wizard/Gift Wizard/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Gift Wizard
+//
+//  Created by Balbina Santana on 6/29/15.
+//  Copyright (c) 2015 Balbina Santana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
